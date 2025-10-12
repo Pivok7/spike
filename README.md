8f93f99d7ee600bbdab4210f95db077f1fe796ee
Pivok <pivoc@protonmail.com>
2025-10-12 14:29:10 +0200

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# spike
+
+Experimental command line interface
+
+
+## Building
+
+You will need:
+
+* Zig compiler (latest stable version)
+
+
+## Third party libraries used in this project
+
+* https://github.com/Gota7/zig-sdl3 <br>
+Licensed under the MIT License.
---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "spike",
+        .root_module = exe_mod,
+    });
+
+    const sdl3_dep = b.dependency("sdl3", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("sdl3", sdl3_dep.module("sdl3"));
+
+    b.installArtifact(exe);
+
+    // Run step
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_cmd.step);
+}
